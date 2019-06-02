200f2d7d4dd0365ab03e3b1b342296fad945755e
Fiyin Kolawole <kolawolemmanuel@hotmail.com>
2019-06-02 07:58:51 +0100

    166397657-feature(layout): Add Tabbed Display
    - Creates components to handle tabbed display
    
    Finishes #166397657

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,7 @@ import Footer from '../components/Landing page/Footer';
 import Services from '../components/Landing page/Services';
 import More from '../components/Landing page/More';
 import Admin from '../components/Admin';
-import User from '../components/User';
+import Tab from './Tab';
 
 const landingPage = () => (
   <div>
@@ -25,7 +25,7 @@ const App = () => (
     <Switch>
       <Route path="/" exact component={landingPage} />
       <Route path="/admin" exact component={Admin} />
-      <Route path="/user" exact component={User} />
+      <Route path="/tab" exact component={Tab} />
     </Switch>
   </Router>
 );
